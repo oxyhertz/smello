ab8eaa25759b582ae046a3b4f15f0cb020b5dde8
DeeM <49118076+oxyhertz@users.noreply.github.com>
2022-03-22 23:59:05 +0200

    board list

--- src/components/board-list.vue
+++ src/components/board-list.vue
@@ -1,6 +1,11 @@
 <template>
+    <h3>Starred Boards</h3>
     <ul class="board-list">
-        <board-preview @toggleFavorite="toggleFavorite" v-for="board in boards" :board="board" :key="board._id" @click="moveToBoard(board._id)" />
+        <board-preview @toggleFavorite="toggleFavorite" v-for="board in starredBoards" :board="board" :key="board._id" @click="moveToBoard(board._id)" />
+    </ul>
+    <h3>Other Boards</h3>
+    <ul class="board-list">
+        <board-preview @toggleFavorite="toggleFavorite" v-for="board in unStarredBoards" :board="board" :key="board._id" @click="moveToBoard(board._id)" />
     </ul>
     
 </template>
@@ -10,6 +15,7 @@ import boardPreview from "./board-preview.vue"
 
 export default{
     name: 'board-list',
+    emits: ["toggleFavorite"],
     props:{
         boards: Array,
     },
@@ -27,6 +33,14 @@ export default{
             this.$emit('toggleFavorite', id);
 
         }
+    },
+    computed:{
+        starredBoards(){
+            return this.boards.filter(board => board.isFavorite)
+        },
+        unStarredBoards(){
+            return this.boards.filter(board => !board.isFavorite)
+        },
     },
     components:{
         boardPreview
--- src/components/board-preview.vue
+++ src/components/board-preview.vue
@@ -11,6 +11,7 @@
 <script>
 export default{
     name: 'board-preview',
+    emits: ["toggleFavorite"],
     props:{
         board:Object
     },
