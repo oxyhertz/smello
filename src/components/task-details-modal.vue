eea410badb316dc5dfb8dbf9230274f4f1a3c7cd
DeeM <49118076+oxyhertz@users.noreply.github.com>
2022-03-24 10:39:23 +0200

    icons

--- src/components/task-details-modal.vue
+++ src/components/task-details-modal.vue
@@ -1,25 +1,45 @@
 <template>
     <section class="task-details-modal">
         <div class="header">
+            <span class="icon"></span>
             <input type="text" v-model="taskToEdit.title" class="task-details-title">
-        {{taskToEdit}}
         </div>
         <section class="task-detail-main">
             <div class="task-content">
                 <div class="task-description">
-                        <div>
-                            <h3>Description</h3>
+                        <div class="block-title">
+                             <span class="icon"></span>
+                             <h3>Description</h3>
                         </div>
                         <textarea v-model="taskToEdit.description" spellcheck="false" placeholder="Add a more detailed description" class="description-textarea"></textarea>
+                        <div class="save-close-description">
+                            <button class="save">Save</button>
+
+                        </div>
                 </div>
                 <div class="activity-show-details">
-                    <h3>Activity</h3>
+                       <div class="block-title">
+                            <span class="icon"></span>
+                            <h3>Activity</h3>
+                        </div>
                     <button>Show Details</button>
                 </div>
             </div>
-            <div>
-
-                hasdsa
+            <div class="actions-btns-container">
+                <h3>Suggested</h3>
+                <button>Join</button>
+                <h3>Add To card</h3>
+                <button>Members</button>
+                <button>Labels</button>
+                <button>Checklist</button>
+                <button>Dates</button>
+                <button>Attachments</button>
+                <button>Location</button>
+                <button>cover</button>
+                <h3>Actions</h3>
+                <button>Copy</button>
+                <button>Archive</button>
+                <button>Share</button>
             </div>
         </section>
     </section>
@@ -47,3 +67,4 @@ export default {
     }
 }
 </script>
+
