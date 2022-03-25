79d84e0c26b48e706f44d98be82df3c0151a4fa8
Eden Avidan <edenaavidan@gmail.com>
2022-03-25 11:53:12 +0300

    fixed add group not opening

--- src/components/add-group.vue
+++ src/components/add-group.vue
@@ -1,14 +1,14 @@
 <template>
-    <section class="add-group">
-        <span v-if="!isFocused" @click="isFocused = true">
-        <span class="icon"></span>
-        Add another list
+    <section class="add-group" @click="isFocused = true">
+        <span v-if="!isFocused">
+            <span class="icon"></span>
+            Add another list
         </span>
-        
+
         <section v-else>
             <input type="text" v-model="groupTitle" v-focus placeholder="Enter list title..." />
             <button @click="addGroup">Add list</button>
-            <button @click="isFocused = false">x</button>
+            <button @click.stop="isFocused = false">x</button>
         </section>
     </section>
 </template>
