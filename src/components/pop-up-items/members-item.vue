fe327ad43f20acc7158ae9bd6b7e9a2a62ed5720
dvir <dvir6g6@gmail.com>
2022-03-25 21:03:50 +0300

    members pop-up

--- src/components/pop-up-items/members-item.vue
+++ src/components/pop-up-items/members-item.vue
@@ -1,17 +1,22 @@
-<template v-if="members">
+<template >
+<section v-if="board" class="members-item">
+  <input type="text" placeholder="Search members">
   <h3>Board members</h3>
   <ul>
-    <li v-for="member in members" :key="member._id">
+    <li v-for="member in board.members" :key="member._id" class="member flex align-items">
       <div>
-           <avatar :size="28" color="white" :name="member.fullname"></avatar>
+           <avatar :size="31" color="white" :name="member.fullname"></avatar>
       </div>
+      <span class="name">{{member.fullname}}</span>
+      <span class="username">({{member.username}})</span>
     </li>
   </ul>
+  </section>
 </template>
 
 <script>
 export default {
-  props:['members']
+  props:['board']
 };
 </script>
 
