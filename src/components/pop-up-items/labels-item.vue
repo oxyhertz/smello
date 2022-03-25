ac17479c83d12aae50de94298f7dce14ebe08370
dvir <dvir6g6@gmail.com>
2022-03-25 15:32:58 +0300

    board-as-prop/pop-up

--- src/components/pop-up-items/labels-item.vue
+++ src/components/pop-up-items/labels-item.vue
@@ -1,24 +1,62 @@
 <template>
-  <div v-if="board" class="labels-item">
+  <div v-if="board && !isCreating" class="labels-item">
     <input type="text" placeholder="Search labels..." />
     <h3>Labels</h3>
     <ul>
-      <li
-        class="label"
-        v-for="label in board.labels"
-        :key="label._id"
-        :style="{ 'background-color': label.color }"
-      >
-        <span>{{ label.title }}</span>
-      <div class="hover-marker"></div>
+      <li class="label" v-for="label in board.labels" :key="label._id">
+        <span :style="{ 'background-color': label.color }" class="label-txt">{{
+          label.title
+        }}</span>
+        <div :class="{ 'hover-marker': null }"></div>
+        <span @click="editLabel" class="edit-labels"></span>
       </li>
     </ul>
+    <button @click="isCreating = true" class="create-label-btn">
+      Create a new label
+    </button>
   </div>
+  <section v-else>
+    <h3>Select a color</h3>
+    <color-picker @updateColor="updateBgColor"></color-picker>
+    <div class="labels-actions flex space-between">
+      <button class="save">Save</button>
+      <button class="delete-label">Delete</button>
+    </div>
+  </section>
 </template>
+
 <script>
+import colorPicker from "../../components/color-picker.vue";
 export default {
   props: ["board"],
+  data() {
+    return {
+      isCreating: null,
+    };
+  },
+  methods: {
+    editLabel() {},
+    addLabel(color, title) {
+      const item = {
+        type: "labels",
+        item: {
+          color,
+          title,
+        },
+      };
+      this.$emit("addItem", item);
+    },
+  },
+  components: {
+    colorPicker,
+  },
 };
 </script>
 <style>
+.create-label-btn {
+  padding: 28px;
+  text-align: center;
+  display: flex;
+  justify-content: center;
+}
 </style>
