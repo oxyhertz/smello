f877108d244314b50680c0fd83523d56074c59eb
DeeM <49118076+oxyhertz@users.noreply.github.com>
2022-03-26 18:35:02 +0300

    te

--- src/components/board-preview.vue
+++ src/components/board-preview.vue
@@ -1,30 +1,31 @@
 <template>
     <li :style="boardStyle" v-if="board">
-        {{board.title}}
+        {{ board.title }}
         <section class="star" @click.stop="toggleFavorite(board._id)">
             <i v-if="!board.isFavorite" class="fa-regular fa-star"></i>
             <i v-else class="fa-solid fa-star"></i>
+            <div class></div>
         </section>
     </li>
 </template>
 
 <script>
-export default{
+export default {
     name: 'board-preview',
     emits: ["toggleFavorite"],
-    props:{
-        board:Object
+    props: {
+        board: Object
     },
-    methods:{
-        toggleFavorite(id){
+    methods: {
+        toggleFavorite(id) {
             this.$emit('toggleFavorite', id);
         }
     },
-    computed:{
-        boardStyle(){
+    computed: {
+        boardStyle() {
             let style = {
                 'background-color': this.board.style.bgColor ? this.board.style.bgColor : '',
-                'background-image': this.board.style.bgImg ? `url(${this.board.style.bgImg})`  : ''
+                'background-image': this.board.style.bgImg ? `url(${this.board.style.bgImg})` : ''
             }
             return style
         }
--- src/components/task-preview.vue
+++ src/components/task-preview.vue
@@ -1,18 +1,19 @@
 <template>
   <Draggable v-if="task">
     <section class="task-preview" :style="taskStyle">
-      <div class="dark-wrap" v-if="this.task.cover?.imgUrl && this.task.cover.type === 'inline' "></div>
-      <div class="task-preview-cover" :style="coverStyle"  v-if="task.cover?.type === 'header'"></div>
-        <p class="title" :class="{'pos-absolute': this.task.cover?.type === 'inline' && this.task.cover?.imgUrl}">{{ task.title }}</p>
+      <div class="dark-wrap" v-if="this.task.cover?.imgUrl && this.task.cover.type === 'inline'"></div>
+      <div class="task-preview-cover" :style="coverStyle" v-if="task.cover?.type === 'header'"></div>
+      <p
+        class="title"
+        :class="{ 'pos-absolute': this.task.cover?.type === 'inline' && this.task.cover?.imgUrl }"
+      >{{ task.title }}</p>
       <div class="icon-container flex" v-if="this.task.cover?.type !== 'inline'">
         <div v-if="task.dueDate">
-                <span
-                  class="preview-icon"
-                  :style="{ 'font-size': 12 + 'px' }"
-                  :class="{ completed: task.status === 'completed', overdue: task.status === 'overdue' }"
-                >
-                  {{ date }}</span
-                >
+          <span
+            class="preview-icon"
+            :style="{ 'font-size': 12 + 'px' }"
+            :class="{ completed: task.status === 'completed', overdue: task.status === 'overdue' }"
+          >{{ date }}</span>
         </div>
         <div class="preview-icon" v-if="task.description">
           <span class="icon-description"></span>
@@ -21,15 +22,19 @@
           <span class="icon-attachment"></span>
           <p>{{ task.attachments?.length }}</p>
         </div>
-        <div class="preview-icon" v-if="task.checklists?.length && numOfTodos" :class="{completed: tasksDone === numOfTodos}">
+        <div
+          class="preview-icon"
+          v-if="task.checklists?.length && numOfTodos"
+          :class="{ completed: tasksDone === numOfTodos }"
+        >
           <span class="icon-checklist"></span>
           <p>{{ tasksDone }} / {{ numOfTodos }}</p>
         </div>
-         <div class="preview-icon members" v-if="task.members?.length">
-              <div v-for="member in task.members" :key="member._id">
-                <avatar :size="28" color="white" :name="member.fullname"></avatar>
-              </div>
-            </div>
+        <div class="preview-icon members" v-if="task.members?.length">
+          <div v-for="member in task.members" :key="member._id">
+            <avatar :size="28" color="white" :name="member.fullname"></avatar>
+          </div>
+        </div>
       </div>
       <!-- <button @click.stop="removeTask">Delete</button> -->
     </section>
@@ -72,27 +77,27 @@ export default {
     numOfTodos() {
       return this.task.checklists.reduce((acc, { todos }) => acc + todos.length, 0)
     },
-    coverStyle(){
-      if(this.task.cover?.color && !this.task.cover.imgUrl)
-        return {'background-color':this.task.cover.color};
-      if(this.task.cover?.imgUrl){
-        return {'background-image': 'url('+this.task.cover.imgUrl+')','height':'260px'}
+    coverStyle() {
+      if (this.task.cover?.color && !this.task.cover.imgUrl)
+        return { 'background-color': this.task.cover.color };
+      if (this.task.cover?.imgUrl) {
+        return { 'background-image': 'url(' + this.task.cover.imgUrl + ')', 'height': '260px' }
       }
     },
-    taskStyle(){
+    taskStyle() {
       console.log(this.task.cover)
-      if(this.task.cover?.type === 'inline'){
-          const style = {
-            'background-color': this.task.cover.color,
-            'font-weight' : 500,
-            'font-size' : '16px',
-            'padding-top' : '30px',
-            'background-image': 'url('+this.task.cover.imgUrl+')',
-            'min-height': this.task.cover.imgUrl ? '250px' : '',
-             'color': '#fff',
-             'padding-top': this.task.cover.imgUrl ? '215px' : '30px',
-          }
-          return style
+      if (this.task.cover?.type === 'inline') {
+        const style = {
+          'background-color': this.task.cover.color,
+          'font-weight': 500,
+          'font-size': '16px',
+          'padding-top': '30px',
+          'background-image': 'url(' + this.task.cover.imgUrl + ')',
+          'min-height': this.task.cover.imgUrl ? '250px' : '',
+          'color': '#fff',
+          'padding-top': this.task.cover.imgUrl ? '215px' : '30px',
+        }
+        return style
       }
     }
   }
