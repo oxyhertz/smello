b9e8a23e2fc3d21185d275ec2c51dc87826b4f9b
DeeM <49118076+oxyhertz@users.noreply.github.com>
2022-03-26 13:54:27 +0300

    Board bg

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
-  <app-header/>
-  <router-view />
+  <div class="wrapper" :style="{'background-color': currBoard}">
+    <app-header/>
+    <router-view />
+   </div> 
 </template>
 
 <script>
@@ -8,13 +10,36 @@ import appHeader from './components/main-header.vue'
 import {userManageService} from './services/user-manage-service.js'
 export default {
   name: 'app',
+  data(){
+    return{
+      color:'red',
+      bg:'beige'
+    }
+  },
   created() {
     this.$store.dispatch({ type: 'loadBoards' })
     this.$store.dispatch({ type: 'loadUsers' })
     userManageService.query()
+    console.log(this.$store.getters.currBoard?.style.bgColor)
+
+  },
+  computed:{
+    currBoard(){
+      console.log(this.$store.getters?.currBoard?.style?.bgColor)
+      return this.$store.getters.currBoard?.style?.bgColor
+    }
   },
   components: {
     appHeader,
   },
 }
+
 </script>
+
+<style >
+  #app{
+    /* background-color: red; */
+    /* background-color: v-bind('color'); */
+    /* background:'yellow'; */
+  }
+</style>
