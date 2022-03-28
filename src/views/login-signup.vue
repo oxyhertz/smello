ffa6ab04af2221981ced485914403463c4c50cf2
Eden Avidan <edenaavidan@gmail.com>
2022-03-28 19:24:59 +0300

    added login styling

--- src/views/login-signup.vue
+++ src/views/login-signup.vue
@@ -1,24 +1,32 @@
 <template>
-    <header>
-        <h1>Drello</h1>
-    </header>
-    <main class="login-container">
-        <h3>Log in to Drello</h3>
-        <form @submit.prevent="onLoginSignup">
-            <input type="text" v-model="user.username" placeholder="Enter Username" />
-            <input
-                v-if="!isLogin"
-                v-model="user.fullname"
-                type="text"
-                placeholder="Enter Full Name"
-            />
-            <input type="password" v-model="user.password" placeholder="Enter password" />
-            <button class="login-btn">Log in</button>
-        </form>
+    <section class="main-container">
+        <header>
+            <span class="icon-trello"></span>
+            <h1>Drello</h1>
+        </header>
+        <main class="login-container">
+            <h2>Log in to Drello</h2>
+            <form @submit.prevent="onLoginSignup">
+                <input type="text" v-model="user.username" placeholder="Enter Username" />
+                <input
+                    v-if="!isLogin"
+                    v-model="user.fullname"
+                    type="text"
+                    placeholder="Enter Full Name"
+                />
+                <input type="password" v-model="user.password" placeholder="Enter password" />
+                <button class="login-btn">{{ loginSignupBtnTxt }}</button>
+                <div class="login-option-sep">Or</div>
+                <button class="login-with">
+                    <span class="google-icon"></span>
+                    <span class="btn-txt">Continue with Google</span>
+                </button>
+            </form>
 
-        <hr />
-        <p @click="toggleLoginSignup">{{ loginSignupTxt }}</p>
-    </main>
+            <hr />
+            <p @click="toggleLoginSignup" class="change-form-link">{{ loginSignupLinkTxt }}</p>
+        </main>
+    </section>
 </template>
 
 
@@ -56,7 +64,10 @@ export default {
         }
     },
     computed: {
-        loginSignupTxt() {
+        loginSignupBtnTxt() {
+            return this.isLogin ? 'Login' : 'Signup';
+        },
+        loginSignupLinkTxt() {
             return this.isLogin ? 'Sign up for an account' : 'Already have an account? Log in';
         }
     }
