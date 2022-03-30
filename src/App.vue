cf596ed5a1e2d2da1d34fb3fc1a156f487913a36
dvir <dvir6g6@gmail.com>
2022-03-30 16:47:10 +0300

    location

--- src/App.vue
+++ src/App.vue
@@ -7,15 +7,27 @@
 
 <script>
 import appHeader from './components/main-header.vue'
+import { socketService } from './services/socket-service';
 import { userService } from './services/user-service.js';
 
 
 export default {
 	name: 'app',
 	async created() {
+		socketService.off('notify activity')
+		socketService.on('notify activity', this.notifyActivity)
 		await this.$store.dispatch({ type: 'loadUsers' })
 		if (!this.miniUser) await this.$store.dispatch({ type: 'login', cred: userService.getGuestUser() });
 		await this.$store.dispatch({ type: 'loadBoards' })
+	},
+	methods: {
+		notifyActivity(activity) {
+			if (activity.toMember?._id) {
+				if (userService.getLoggedinUser()._id === activity.toMember._id) {
+					// notify()
+				}
+			} else console.log('everybody')
+		},
 	},
 	computed: {
 		miniUser() {
